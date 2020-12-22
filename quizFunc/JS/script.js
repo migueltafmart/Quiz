fb92229f4bab973f91aa94780ac63ef866f0f89b
Miguel <migueltafmart@gmail.com>
2020-12-22 09:15:29 +0100

    221220 9:14

--- quizFunc/JS/script.js
+++ quizFunc/JS/script.js
@@ -1,29 +1,69 @@
-const answers = {
-    p1: "azul",
-    p2: "verde",
-    p3: "amarillo",
-    p4: "rojo",
-    p5: "verde"
-}
-
-let aciertos = 0;
-function chequear(num,respuesta){
-    //Pregunta 1
-    let options = document.querySelectorAll(`fieldset#p${num} input`);
-    for (let i = 0; i < options.length; i++) {
-        if (options[i].checked && options[i].value === respuesta)
-            ++aciertos;
-            console.log(aciertos);
-    }
-}
-
-function checkAnswers(e) {
-    e.preventDefault();
-    chequear(1,answers.p1)
-    chequear(2,answers.p2)
-    chequear(3,answers.p3)
-    chequear(4,answers.p4)
-    chequear(5,answers.p5)
-    //e.target.submit();
+const q1 = {
+    name: "viajes",
+    legend: "¿A dónde te gustaría ir de viaje?",
+    answers: [
+        {label: "Playa", value:"playa"},
+        {label:"Montaña", value:"montana"},
+        {label:"Al pueblo", value:"pueblo"},
+        {label:"Cualquier sitio en el que me pueda quejar", value: "quejar"}
+    ],
+    correct:"playa",
 };
-//document.getElementById("quiz").addEventListener("submit", event => checkAnswers(event));
+
+const q2 = {
+    name: "cumple",
+    legend: "¿Qué me vas a regalar por mi cumple?",
+    answers: [
+        {label: "Ropa", value:"ropa"},
+        {label:"Comida", value:"comida"},
+        {label:"Mimos", value:"mimos"},
+        {label:"¿Tu cumple fue ayer? Ups", value: "nada"}
+    ],
+    correct:"comida",
+};
+
+const questions = [ q1, q2 ];
+
+function printQuestion (question) {
+    let form = document.createElement("form");
+    form.setAttribute("name", question.name);
+    let legend = document.createElement("legend");
+    let labelText = document.createTextNode(question.legend);
+    legend.appendChild(labelText);
+    form.appendChild(legend);
+    question.answers.forEach(option => {
+        let input = document.createElement("input");
+        input.setAttribute("type", "radio");
+        input.setAttribute("value", option.value);
+        input.setAttribute("name", question.name)
+        let label = document.createElement("label");
+        let labelText = document.createTextNode(option.label);
+        label.appendChild(labelText);
+        label.appendChild(input);
+        form.appendChild(label);
+        document.querySelector("main").appendChild(form);
+    });
+    let nextButton = document.createElement("input");
+    nextButton.setAttribute("type", "submit");
+    nextButton.setAttribute("value", "Next");
+    form.appendChild(nextButton);
+
+    function checkQuestion (event) {
+        event.preventDefault();
+        let userInput = document.querySelector(`input[type=radio][name=${question.name}]:checked`);
+        if (userInput === null){
+            console.error("Porfi responde");
+        }else if(userInput.value === question.correct){
+            console.log("Respuesta Correcta!");
+        }else{
+            console.warn("Respuesta Incorrecta");
+        }
+    }
+    form.addEventListener("submit", (event) => checkQuestion(event));
+    return form;
+}
+
+// @param questions {Array of JSONS}
+function printQuestions (questions) {
+ return questions.map(question => printQuestion(question))
+}
